078bada90dd3afead0065b4a504fd5109314b717
tranhuuthiennguyen <kevinnguyen31415@gmail.com>
2023-01-03 00:26:43 +0700

    deleted

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/huuthien/challenge/2023/day1/target/debug/day1: /home/huuthien/challenge/2023/day1/src/main.rs
--- day1/target/debug/deps/day1-eef1838ca7838884.d
+++
@@ -1,5 +0,0 @@
-/home/huuthien/challenge/2023/day1/target/debug/deps/day1-eef1838ca7838884.rmeta: src/main.rs
-
-/home/huuthien/challenge/2023/day1/target/debug/deps/day1-eef1838ca7838884.d: src/main.rs
-
-src/main.rs:
