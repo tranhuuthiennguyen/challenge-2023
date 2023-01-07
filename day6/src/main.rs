e97beac4e5adbc5172771e5a70a92c580a57b25e
tranhuuthiennguyen <kevinnguyen31415@gmail.com>
2023-01-07 13:59:52 +0700

    solved day 6 using stack

---
+++ day6/src/main.rs
@@ -0,0 +1,60 @@
+use std::vec::Vec;
+
+struct Stack<T> {
+    stack: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    fn new() -> Self {
+        Stack { stack: Vec::new() }
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self.stack.pop()
+    }
+
+    fn push(&mut self, item: T) {   
+        self.stack.push(item)
+    }
+
+    fn is_empty(&self) -> bool {
+        self.stack.is_empty()
+    }
+
+    fn length(&self) -> usize {
+        self.stack.len()
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.stack.last()
+    }
+}
+
+fn is_valid_paren(string: &str) -> bool {
+    let mut stack : Stack<char> = Stack::new();
+
+    let char_vec : Vec<char> = string.chars().collect();
+
+    for ch in char_vec {
+        if ch == '(' || ch == '[' || ch == '{' {
+            stack.push(ch);
+        } else {
+            let top = stack.peek().unwrap();
+            if (*top == ')' && ch != '(') ||
+                (*top == '[' && ch != ']' ||
+                (*top == '{' && ch != '}')) {
+                    continue;
+                }
+            else {
+                stack.pop();
+            }
+        }
+    }
+
+    stack.is_empty()
+}
+
+fn main() {
+    let output = is_valid_paren(&"([()})");
+    println!("{output}");
+}
