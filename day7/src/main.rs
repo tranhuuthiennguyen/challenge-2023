1d113ef38c71260724441664d1da2a22e2141c92
tranhuuthiennguyen <kevinnguyen31415@gmail.com>
2023-01-07 14:21:03 +0700

    solved day 7 by comparing reversed order

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,12 @@
+use std::vec::Vec;
+
+fn is_palindromic(n: &i32) -> bool {
+    let num_as_str = n.to_string();
+    let reveresed = num_as_str.chars().rev().collect::<String>();
+    num_as_str == reveresed
+}
+
 fn main() {
-    println!("Hello, world!");
+    let output = is_palindromic(&-101);
+    println!("{output}");
 }
